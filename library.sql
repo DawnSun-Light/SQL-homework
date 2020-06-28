cbdb5305f3de973672a6a50bdfe966c4498d93b9
罗东旭 <dxluo.xdu@gmail.com>
2020-06-28 10:23:49 +0800

    增加测试数据

--- library.sql
+++ library.sql
@@ -13,9 +13,9 @@ CREATE TABLE IF NOT EXISTS book (
     remaining_quantity INT
 );
 
-INSERT INTO book (ISBN,book_name,press,author,book_classification,year_of_publication,remaining_quantity) VALUES ('9787040406641','数据库系统概论','高等教育出版社','萨师煊','数据库','2015',3);
-INSERT INTO book (ISBN,book_name,press,author,book_classification,year_of_publication,remaining_quantity) VALUES ('9787040174014','信号与线性系统分析','高等教育出版社','吴大正','信号与系统','2015',5);
-INSERT INTO book (ISBN,book_name,press,author,book_classification,year_of_publication,remaining_quantity) VALUES ('9787560605814','微型计算机原理及应用','西安电子科技大学出版社','李伯成','微机原理','2014',4);
+INSERT INTO book (ISBN,book_name,press,author,book_classification,year_of_publication,remaining_quantity) VALUES ('9787040406641','数据库系统概论','高等教育出版社','萨师煊','数据库','2014',30);
+INSERT INTO book (ISBN,book_name,press,author,book_classification,year_of_publication,remaining_quantity) VALUES ('9787040174014','信号与线性系统分析','高等教育出版社','吴大正','信号与系统','2005',50);
+INSERT INTO book (ISBN,book_name,press,author,book_classification,year_of_publication,remaining_quantity) VALUES ('9787560605814','微型计算机原理及应用','西安电子科技大学出版社','李伯成','微机原理','2008',40);
 
 CREATE TABLE IF NOT EXISTS borrowing (
     internal BOOLEAN,
@@ -26,6 +26,8 @@ CREATE TABLE IF NOT EXISTS borrowing (
     fine INT DEFAULT 0
 );
 
+INSERT INTO borrowing (internal,ISBN,ID_number,borrow_time) VALUES (TRUE,9787040406641,18180100109, 20200520131420);
+
 CREATE TABLE IF NOT EXISTS internal_staff (
     ID_number CHAR(18),
     full_name CHAR(20),
@@ -34,8 +36,8 @@ CREATE TABLE IF NOT EXISTS internal_staff (
 );
 
 INSERT INTO internal_staff (ID_number,full_name) VALUES ('18180100109','罗东旭');
-INSERT INTO internal_staff (ID_number,full_name) VALUES ('18180100137','王中正');
-INSERT INTO internal_staff (ID_number,full_name) VALUES ('18180100052','李伯涵');
+INSERT INTO internal_staff (ID_number,full_name,number_of_books_borrowed) VALUES ('18180100137','王中正',5);
+INSERT INTO internal_staff (ID_number,full_name,overdue) VALUES ('18180100052','李伯涵',1);
 
 CREATE TABLE IF NOT EXISTS external_staff (
     ID_number CHAR(18),
